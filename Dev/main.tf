4c69fc1a8d352e3dd01a0f8d9516fe1d0b5d738c
ashokgitproj <92667442+ashokgitproj@users.noreply.github.com>
2025-10-05 23:49:56 +0530

    Adding changes

--- Dev/main.tf
+++ Dev/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_resource_group" "rg1" {
-  name     = var.rg_name
+  name     = var.rgname
   location = var.location
 }
 module "ServicePrincipal" {
@@ -11,3 +11,26 @@ module "ServicePrincipal" {
   depends_on = [
     azurerm_resource_group.rg1
   ]
+resource "azurerm_role_assignment" "rolespn" {
+  scope                = /subscriptions/${var.SUB_ID}
+  role_definition_name = "Contributor"
+  principal_id         = module.ServicePrincipal.service_principal_id
+
+  depends_on = [
+    module.ServicePrincipal
+  ]
+}
+
+module "KeyVault" {
+  source              = "../module/keyvault"
+  key_vault_name      = "var.keyvault_name"
+  resource_group_name = "var.rgname"
+  location            = "var.location"
+  serviceprincipal_name     = var.serviceprincipal_name
+  serviceprincipal_tenant_id = module.ServicePrincipal.service_principal_tenant_id
+  serviceprincipal_object_id = module.ServicePrincipal.service_principal_object_id
+
+  depends_on = [
+    module.ServicePrincipal
+  ]
+}
