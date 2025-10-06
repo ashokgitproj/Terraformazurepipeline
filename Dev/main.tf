569c088bd2b61e881950779af17350ae55567e26
ashokgitproj <92667442+ashokgitproj@users.noreply.github.com>
2025-10-06 18:29:31 +0530

    Changes

--- Dev/main.tf
+++ Dev/main.tf
@@ -6,7 +6,7 @@ module "ServicePrincipal" {
   source = "../modules/ServicePrincipal"
 
   # Pass required variables (adjust these based on your module's variables.tf)
-  serviceprincipal_name     = var.serviceprincipal_name         # example variable: service principal name
+  service_principal_name     = var.service_principal_name         # example variable: service principal name
   
   depends_on = [
     azurerm_resource_group.rg1
@@ -21,16 +21,48 @@ resource "azurerm_role_assignment" "rolespn" {
   ]
 }
 
-module "KeyVault" {
+module "keyvault" {
   source              = "../module/keyvault"
   key_vault_name      = "var.keyvault_name"
   resource_group_name = "var.rgname"
   location            = "var.location"
-  serviceprincipal_name     = var.serviceprincipal_name
+  service_principal_name     = var.service_principal_name
   serviceprincipal_tenant_id = module.ServicePrincipal.service_principal_tenant_id
   serviceprincipal_object_id = module.ServicePrincipal.service_principal_object_id
 
   depends_on = [
     module.ServicePrincipal
   ]
+}
+
+resource "azurerm_key_vault_secret" "example" {
+  name         = module.ServicePrincipal.client_id
+  value        = module.ServicePrincipal.client_secret
+  key_vault_id = module.keyvault.keyvault_id
+
+  depends_on = [
+    module.keyvault
+  ]
+}
+
+module "aks" {
+  source = "../modules/aks"
+
+  resource_group_name     = var.rg1
+  location                = var.location
+  cluster_name            = var.cluster_name
+  service_principal_name     = var.service_principal_name
+  client_id               = module.ServicePrincipal.client_id
+  client_secret           = module.ServicePrincipal.client_secret
+  node_pool_name          = var.node_pool_name
+
+  depends_on = [
+    module.ServicePrincipal
+  ]
+}
+
+resource "local_file" "kubeconfig" {
+  depends_on   = [module.aks]
+  filename     = "./kubeconfig"
+  content      = module.aks.config
 }
